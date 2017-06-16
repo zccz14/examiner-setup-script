9184890bb093e5a60097982f88598ed73e92ef9c
Jack Q <qiaobo@outlook.com>
2017-06-17 05:27:25 +0800

    add httpd config

---
+++ server/config-httpd.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# disable default site
+[ -e /etc/apache2/sites-enabled/000-default.conf ] && a2dissite 000-default.conf
+[ -e /etc/apache2/sites-enabled/100-exam.conf ] && a2dissite 100-exam.conf
+
+# create exam site
+rm -rf /var/www/exam
+mkdir /var/www/exam
+cp -r ./html/* /var/www/exam/
+chown -Rf root:root /var/www/exam
+cp ./100-exam.conf /etc/apache2/sites-available/
+a2ensite 100-exam.conf
+
+# Enable CGI
+[ -e /etc/apache2/mods-enabled/cgi.load ] || a2enmod cgi
+
+service apache2 reload 
---
+++ server/html/cgi-bin/score.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+echo 'Content-type: text/html'
+echo ''
+
+# Header section
+cat << EOF
+<!doctype html>
+<html lang=en>
+  <head><title>Score</title></head>
+  <body>
+    <div>
+      <h1>Score Query</h1>
+      <hr />
+    </div>
+    <form method='post'>
+      <label for='name'>Name:</label>
+      <input required type='text' name='name' id='name'>
+      <br>
+      <label for='pass'>Password</label>
+      <input required type='password' name='pass' id='pass'>
+      <br>
+      <input type='submit' value='query' />
+      <input type='reset' value='reset' /
+    </form>
+EOF
+
+score=""
+error_msg=""
+
+if [ $REQUEST_METHOD -eq 'POST' ]; then
+  read POST_STRING
+  addr=REMOTE_ADDR
+fi
+
+echo '<div class="result">'
+if [ "$score" = "" ]; then 
+  echo 'enter your login name (stu + student ID) and password'
+  echo $error_msg
+else 
+  echo 'your score is: ' $score
+fi
+echo '</div>'
+
+# Footer Section
+cat << EOF
+  </body>
+</html>
+EOF
+
