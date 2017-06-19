f2cc69ae3992335851905c6a244f9b9a128dff23
Jack Q <qiaobo@outlook.com>
2017-06-20 02:16:22 +0800

    check answer and publish score

--- server/answer-check.sh
+++ server/answer-check.sh
@@ -1,1 +1,44 @@
 #!/usr/bin/env bash
+
+source ../config/server-answer.conf
+
+answer=`echo $EXAM_ANSWER | sed -e 's/\s//g'`
+
+function calcScore(){
+	paper=$1
+	answerSep=`echo $answer | sed -e 's/[A-Z]/\0 /g'`
+	paperSep=`echo $paper | sed -e 's/\s//g' | sed -e 's/[A-Z]/\0 /g'`
+	count=`echo $answer | tr -d '\n' | wc -c`
+	rightCount=0
+	for (( i = 1; i <= $count; i++ )); do
+		exp=`echo $answerSep | cut -d ' ' -f$i`
+		act=`echo $paperSep | cut -d ' ' -f$i`
+		if [[ "$act" = "$exp" ]]; then
+			rightCount=$(($rightCount + 1))
+		fi
+	done
+	echo $((100 * $rightCount / $count))
+}
+
+rm -f ../run/ans.csv
+
+# Process each register entry
+for item in `cat ../run/register/tester-info.csv`; do
+    name=`echo $item | cut -d, -f1`
+    pass=`echo $item | cut -d, -f2 | base64 -d`
+    addr=`echo $item | cut -d, -f3`
+
+    # User Name
+    user_name=stu$name
+    ans_file="../run/answer/$user_name/$user_name"
+    score=0
+    if ! [[ -e $ans_file ]]; then
+    	echo $user_name has no submitted answer
+    else
+    	ans=`cat $ans_file`
+    	score=`calcScore $ans`
+    fi
+    echo stu$item,$score >> ../run/ans.csv
+done
+
+cp ../run/ans.csv /var/www/exam/cgi-bin/ans.csv
--- server/html/cgi-bin/score.sh
+++ server/html/cgi-bin/score.sh
@@ -28,18 +28,30 @@ EOF
 score=""
 error_msg=""
 
-if [ $REQUEST_METHOD -eq 'POST' ]; then
+
+if [[ $REQUEST_METHOD = 'POST' ]]; then
   # read post content from standard input
   read POST_STRING
   addr=REMOTE_ADDR
+  if [[ -e ans.csv ]]; then
+    user=`echo $POST_STRING | sed 's/&/\n/g' | sed 's/=/ /' | awk '/user/{print $2}'`
+    pass=`echo $POST_STRING | sed 's/&/\n/g' | sed 's/=/ /' | awk '/pass/{print $2}'`
+    encPass=`echo $pass | base64`
+    score=`cat ans.csv | grep -F "$user,$encPass,$REMOTE_ADDR," | cut -d , -f4`
+    if [[ "$score" = "" ]]; then
+      error_msg="username, password or IP address mismatch"
+    fi
+  else
+    error_msg="score is not published"
+  fi
 fi
 
 echo '<div class="result">'
 if [ "$score" = "" ]; then 
-  echo 'enter your login name (stu + student ID) and password'
-  echo $error_msg
+  echo '<div>enter your login name (stu + student ID) and password</div>'
+  echo '<div>'$error_msg'</div>'
 else 
-  echo 'your score is: ' $score
+  echo "your score is: $score"
 fi
 echo '</div>'
 
