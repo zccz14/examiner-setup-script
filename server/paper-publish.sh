13d79467f8f82889c141e05c3eb9a8c6df9e1679
Jack Q <qiaobo@outlook.com>
2017-06-19 11:30:08 +0800

    publish paper to www

--- server/paper-publish.sh
+++ server/paper-publish.sh
@@ -3,5 +3,20 @@
 
 # load config
 source ../config/server-network.conf
-declare -x max_index=0
-awk -f ./paper-process.awk ../config/server-paper.conf
+
+# publish html page
+cat << EOF > /var/www/exam/paper.html
+<!doctype html>
+<html>
+<head>
+  <title>Paper</title>
+</head>
+<body>
+`awk -f ./paper-process.awk ../config/server-paper.conf`
+</body>
+</head>
+
+EOF
+
+# publish raw page
+cp ../config/server-paper.conf /var/www/exam/paper.txt
